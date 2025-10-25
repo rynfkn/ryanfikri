da6403bcb4f662d3bc92ec24ba362bce2226beea
rynfkn <ryanfikri.re@gmail.com>
2025-10-25 21:22:55 +0700

    fix:fix path gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,147 +3,147 @@ import { useState } from 'react';
 export default function Gallery() {
     const images = [
         {
-            image_path: 'src/assets/gallery/college/college_1.jpg',
+            image_path: '/gallery/college/college_1.jpg',
             imageCategory: 'College',
             image_description: 'Mandatory photo as ITS Student hehe :3',
         },
         {
-            image_path: 'src/assets/gallery/college/college_2.jpg',
+            image_path: '/gallery/college/college_2.jpg',
             imageCategory: 'College',
             image_description: 'SMAGA tribute at ITS from MIPA2 :P',
         },
         {
-            image_path: 'src/assets/gallery/college/college_3.jpg',
+            image_path: '/gallery/college/college_3.jpg',
             imageCategory: 'College',
             image_description: 'Metanoia in action!',
         },
         {
-            image_path: 'src/assets/gallery/college/college_4.jpg',
+            image_path: '/gallery/college/college_4.jpg',
             imageCategory: 'College',
             image_description: 'Another mandatory photo: 0.5 Selfie',
         },
 
         {
-            image_path: '/src/assets/gallery/bahimaa/Bahimaa_1.jpg',
+            image_path: '/gallery/bahimaa/Bahimaa_1.jpg',
             imageCategory: 'Bahimaa',
             image_description: 'Yeah, Christinity is the main character here :D',
         },
         {
-            image_path: 'src/assets/gallery/bahimaa/Bahimaa_2.jpg',
+            image_path: '/gallery/bahimaa/Bahimaa_2.jpg',
             imageCategory: 'Bahimaa',
             image_description: 'Smile, happy faces that has been through a lot of fun',
         },
         {
-            image_path: 'src/assets/gallery/bahimaa/Bahimaa_3.JPG',
+            image_path: '/gallery/bahimaa/Bahimaa_3.JPG',
             imageCategory: 'Bahimaa',
             image_description: 'Whoever take this pic, Thankyou!',
         },
         {
-            image_path: 'src/assets/gallery/bahimaa/Bahimaa_4.jpg',
+            image_path: '/gallery/bahimaa/Bahimaa_4.jpg',
             imageCategory: 'Bahimaa',
             image_description: 'First visit at SMKN7!',
         },
         {
-            image_path: 'src/assets/gallery/bahimaa/Bahimaa_5.jpeg',
+            image_path: '/gallery/bahimaa/Bahimaa_5.jpeg',
             imageCategory: 'Bahimaa',
             image_description: 'Bahimaa first gath in Samarinda :)',
         },
         {
-            image_path: 'src/assets/gallery/bahimaa/Bahimaa_6.jpeg',
+            image_path: '/gallery/bahimaa/Bahimaa_6.jpeg',
             imageCategory: 'Bahimaa',
             image_description: 'Genzies after studio photo :p',
         },
 
         {
-            image_path: 'src/assets/gallery/kcv/LBE_FP_1.jpeg',
+            image_path: '/gallery/kcv/LBE_FP_1.jpeg',
             imageCategory: 'KCV',
             image_description: 'Visioners after LBE Final Projects',
         },
         {
-            image_path: 'src/assets/gallery/kcv/LBE_Best_Analysis.jpeg',
+            image_path: '/gallery/kcv/LBE_Best_Analysis.jpeg',
             imageCategory: 'KCV',
             image_description: 'Best Analysis team at FP LBE!',
         },
         {
-            image_path: 'src/assets/gallery/kcv/LBE_Best_Presentation.jpeg',
+            image_path: '/gallery/kcv/LBE_Best_Presentation.jpeg',
             imageCategory: 'KCV',
             image_description: 'What a great presentation team adalah pokoknya',
         },
         {
-            image_path: 'src/assets/gallery/kcv/LBE_Best_Implementation.jpeg',
+            image_path: '/gallery/kcv/LBE_Best_Implementation.jpeg',
             imageCategory: 'KCV',
             image_description: 'The best implementation at LBE KCV 2025.  what an effort!',
         },
         {
-            image_path: 'src/assets/gallery/kcv/KCV_1.jpeg',
+            image_path: '/gallery/kcv/KCV_1.jpeg',
             imageCategory: 'KCV',
             image_description: 'KCV 21 First blood seminar!',
         },
         {
-            image_path: 'src/assets/gallery/kcv/KCV_2.jpeg',
+            image_path: '/gallery/kcv/KCV_2.jpeg',
             imageCategory: 'KCV',
             image_description: 'Congrats mba!',
         },
 
         {
-            image_path: 'src/assets/gallery/ITToday/Ittoday_1.jpeg',
+            image_path: '/gallery/ITToday/Ittoday_1.jpeg',
             imageCategory: 'ITtoday',
             image_description: 'We got the third place :D',
         },
         {
-            image_path: 'src/assets/gallery/ITToday/Ittoday_2.jpeg',
+            image_path: '/gallery/ITToday/Ittoday_2.jpeg',
             imageCategory: 'ITtoday',
             image_description: 'idk what im doing but thankyou daffa for taking the pict.',
         },
         {
-            image_path: 'src/assets/gallery/ITToday/Ittoday_3.jpeg',
+            image_path: '/gallery/ITToday/Ittoday_3.jpeg',
             imageCategory: 'ITtoday',
             image_description: 'ITS Team photo at IPB at ITtoday',
         },
         {
-            image_path: 'src/assets/gallery/ITToday/Ittoday_4.jpeg',
+            image_path: '/gallery/ITToday/Ittoday_4.jpeg',
             imageCategory: 'ITtoday',
             image_description: 'Selfie at the coin :3',
         },
 
         {
-            image_path: 'src/assets/gallery/Hology/Hology_1.jpeg',
+            image_path: '/gallery/Hology/Hology_1.jpeg',
             imageCategory: 'Hology',
             image_description: 'After a brutal fight as a dark horses, we got the 2nd place :D',
         },
         {
-            image_path: 'src/assets/gallery/Hology/Hology_2.jpeg',
+            image_path: '/gallery/Hology/Hology_2.jpeg',
             imageCategory: 'Hology',
             image_description: 'Podium selfie :P (we got surprised getting the podium)',
         },
         {
-            image_path: 'src/assets/gallery/Hology/Hology_3.JPG',
+            image_path: '/gallery/Hology/Hology_3.JPG',
             imageCategory: 'Hology',
             image_description: 'photobooth take take take',
         },
         {
-            image_path: 'src/assets/gallery/Hology/Hology_4.jpeg',
+            image_path: '/gallery/Hology/Hology_4.jpeg',
             imageCategory: 'Hology',
             image_description: 'ITS contingent got the throphy at Hology!',
         },
         {
-            image_path: 'src/assets/gallery/Hology/Hology_5.JPG',
+            image_path: '/gallery/Hology/Hology_5.JPG',
             imageCategory: 'Hology',
             image_description: 'yeah i know, UI peps are all monstrous at data mining',
         },
         {
-            image_path: 'src/assets/gallery/Hology/Hology_6.JPG',
+            image_path: '/gallery/Hology/Hology_6.JPG',
             imageCategory: 'Hology',
             image_description: 'Another official podium pict :D',
         },
 
         {
-            image_path: 'src/assets/gallery/dataquest/dataquest_1.jpeg',
+            image_path: '/gallery/dataquest/dataquest_1.jpeg',
             imageCategory: 'Dataquest',
             image_description: '2nd place at Dataquest Airnology!',
         },
         {
-            image_path: 'src/assets/gallery/dataquest/dataquest_2.jpeg',
+            image_path: '/gallery/dataquest/dataquest_2.jpeg',
             imageCategory: 'Dataquest',
             image_description: 'Team photo at Airlagga favs photo place',
         },
