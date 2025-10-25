d4f71566eb73f3dcac1ff8a598b6bdf0a456eac6
rynfkn <ryanfikri.re@gmail.com>
2025-10-25 20:38:53 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { copyFileSync } from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    {
+      name: 'copy-staticwebapp-config',
+      closeBundle() {
+        copyFileSync('staticwebapp.config.json', 'dist/staticwebapp.config.json');
+      },
+    },
   ],
 })
