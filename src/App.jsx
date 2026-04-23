67c74f39be8a177f9904b6042805fcb629c04998
rynfkn <ryanfikri.re@gmail.com>
2026-04-23 23:15:19 +0700

    feat: add page fade transitions with AnimatePresence

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-import './index.css'
+import './index.css';
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Experience from "./pages/Experience";
 import Achievement from "./pages/Achievements";
 import Projects from "./pages/Projects";
 import Gallery from "./pages/Gallery";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
   return (
-
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <Routes location={location}>
           <Route path="/" element={<Home />} />
           <Route path="/experiences" element={<Experience />} />
           <Route path="/achievements" element={<Achievement />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/gallery" element={<Gallery />} />
         </Routes>
-      </Router>
-    </>
-  )
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <AnimatedRoutes />
+    </Router>
+  );
+}
+
+export default App;
